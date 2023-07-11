4e19b269e69475aa9c9130b4b9a46f0e6c1aa516
jinhee <wiostz98kr@gmail.com>
2023-07-11 19:31:40 +0900

    변수 인라인(inline)하기

--- ch6/6-4.js
+++ ch6/6-4.js
@@ -1,4 +1,4 @@
+//100% 정해진 건 없기 때문에 딱 봤을 때 불필요해 보이면 인라인 해주면 됨
 export function isDeliveryFree(anOrder) {
-  let basePrice = anOrder.basePrice;
-  return basePrice > 1000;
+  return anOrder.basePrice > 1000;
 }
