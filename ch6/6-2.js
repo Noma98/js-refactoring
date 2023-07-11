e2b516aaec41f6b19f0b17566f2aec724d13f627
jinhee <wiostz98kr@gmail.com>
2023-07-11 18:45:57 +0900

    함수 인라인(inline)하기

--- ch6/6-2.js
+++ ch6/6-2.js
@@ -1,20 +1,15 @@
+//때때로 함수 본문이 함수의 이름만큼 명확하게 리팩토링될 때가 있음
+//이럴 때는 그 함수를 제거한다. 간접 호출은 유용할 수도 있지만 쓸데없는 간접 호출은 거슬릴 뿐임
+
 // 예제 1
 export function rating(driver) {
-  return moreThanFiveLateDeliveries(driver) ? 2 : 1;
-}
-
-function moreThanFiveLateDeliveries(dvr) {
-  return dvr.numberOfLateDeliveries > 5;
+  return driver.numberOfLateDeliveries > 5 ? 2 : 1;
 }
 
 // 예제 2
 function reportLines(customer) {
-  const lines = [];
-  gatherCustomerData(lines, customer);
-  return lines;
-}
-
-function gatherCustomerData(out, customer) {
-  out.push(['name', customer.name]);
-  out.push(['location', customer.location]);
+  const result = [];
+  result.push(['name', customer.name]);
+  result.push(['location', customer.location]);
+  return result;
 }
