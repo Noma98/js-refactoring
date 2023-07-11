1eea9f373c88ea32a215c896c387e7103b45e078
jinhee <wiostz98kr@gmail.com>
2023-07-11 19:28:36 +0900

    변수 추출(extract)하기

--- ch6/6-3-1.js
+++ ch6/6-3-1.js
@@ -1,8 +1,13 @@
+//긴 표현식을 중간중간 잘라 의미있는 이름을 붙여주면 가독성이 올라감
+
+//quantityDiscount -> discount
+//shippingCost -> shipping
+//부가적인 설명인 도움이 된다면 추가해도 되지만, 짧아도 충분히 문맥상 이해가 간다면 짧은게 더 보기 간결함
+
 export function price(order) {
   // 가격(price) = 기본가격 - 수량할인 + 배송비
-  return (
-    order.quantity * order.itemPrice -
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-    Math.min(order.quantity * order.itemPrice * 0.1, 100)
-  );
+  const basePrice = order.quantity * order.itemPrice;
+  const discount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  const shipping = Math.min(order.quantity * order.itemPrice * 0.1, 100);
+  return basePrice - discount + shipping;
 }
--- ch6/6-3-2.js
+++ ch6/6-3-2.js
@@ -1,20 +1,28 @@
 export class Order {
+  //-> 책은 private 필드를 나타낼 때 밑줄(_)을 사용하지만, 요즘은 #으로 표현
+  #data;
   constructor(aRecord) {
-    this._data = aRecord;
+    // this._data = aRecord;
+    this.#data = aRecord;
   }
 
   get quantity() {
-    return this._data.quantity;
+    return this.#data.quantity;
   }
   get itemPrice() {
-    return this._data.itemPrice;
+    return this.#data.itemPrice;
   }
-
+  //클래스에서도 동일하게 추출
   get price() {
-    return (
-      this.quantity * this.itemPrice -
-      Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 +
-      Math.min(this.quantity * this.itemPrice * 0.1, 100)
-    );
+    return this.basePrice - this.discount + this.shipping;
+  }
+  get basePrice() {
+    return this.quantity * this.itemPrice;
+  }
+  get discount() {
+    return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05;
+  }
+  get shipping() {
+    return Math.min(this.quantity * this.itemPrice * 0.1, 100);
   }
 }
