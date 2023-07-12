af417feedb045e70b6ca73db6da836c8351e6c4f
jinhee <wiostz98kr@gmail.com>
2023-07-12 21:15:32 +0900

    함수 매개변수 객체 만들기

--- ch6/6-8.js
+++ ch6/6-8.js
@@ -1,7 +1,29 @@
-export function readingsOutsideRange(station, min, max) {
-  return station.readings.filter((r) => r.temp < min || r.temp > max);
+/*
+ 함수에 여러 가지 데이터를 전달한다면,  
+- 그 데이터 중 하나의 객체로 묶을 수 있는게 존재하는지 확인
+- 더 나아가 그 객체 안에 유용한 함수를 만들어 호출하는 방식으로 함수의 가독성을 높일 수 없는지 고민 
+ */
+export function readingsOutsideRange(readings, range) {
+  return readings.filter((r) => !range.contains(r.temp));
 }
-
+//데이터와 데이터를 처리하는 로직들이 여기저기 흩어져 있지 않게 관련된 것들을 클래스로 한 곳에 묶어주기
+export class NumberRange {
+  #min;
+  #max;
+  constructor(min, max) {
+    this.#min = min;
+    this.#max = max;
+  }
+  get min() {
+    return this.#min;
+  }
+  get max() {
+    return this.#max;
+  }
+  contains(number) {
+    return number >= this.#min && number <= this.#max;
+  }
+}
 const station = {
   name: 'ZB1',
   readings: [
@@ -12,13 +34,6 @@ const station = {
     { temp: 51, time: '2016-11-10 09:50' },
   ],
 };
-const operationPlan = {
-  temperatureFloor: 51,
-  temperatureCeiling: 53,
-};
+const operationPlan = new NumberRange(51, 53);
 
-readingsOutsideRange(
-  station,
-  operationPlan.temperatureFloor,
-  operationPlan.temperatureCeiling
-);
+console.log(readingsOutsideRange(station.readings, operationPlan));
