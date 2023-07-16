6274504ca3daec51a2f101fd0493a53984307ebe
Noma <wiostz98kr@gmail.com>
2023-07-16 14:50:38 +0900

    함수 쪼개기

--- ch6/6-11-2.js
+++ ch6/6-11-2.js
@@ -1,18 +1,33 @@
 import fs from 'fs';
 
-if (!process.argv[2]) {
-  throw new Error('파일 이름을 입력하세요');
-}
+run(process.argv);
 
-const fileName = `./${process.argv[2]}.json`;
-if (!fs.existsSync(fileName)) {
-  throw new Error('파일이 존재하지 않습니다');
+//1. run 함수를 만들어서 노드의 process 디펜던시를 제거함
+function run(args) {
+  const command = parseCommand(args);
+  countOrders(command);
 }
-
-const rawData = fs.readFileSync(fileName);
-const orders = JSON.parse(rawData);
-if (process.argv.includes('-r')) {
-  console.log(orders.filter((order) => order.status === 'ready').length);
-} else {
-  console.log(orders.length);
+//2. 사용자에게 입력을 받아옴 -> 받아온 값 유효성 검사
+function parseCommand(args) {
+  if (!args[2]) {
+    throw new Error('파일 이름을 입력하세요');
+  }
+  const fileName = `./${args[2]}.json`;
+  if (!fs.existsSync(fileName)) {
+    throw new Error('파일이 존재하지 않습니다');
+  }
+  return {
+    fileName,
+    countReadyOnly: args.includes('-r'),
+  };
+}
+//3. 필요한 로직 처리
+function countOrders({ fileName, countReadyOnly }) {
+  const rawData = fs.readFileSync(fileName);
+  const orders = JSON.parse(rawData);
+  //삼항연산자로 console.log 중복 제거
+  const filtered = countReadyOnly
+    ? orders.filter((order) => order.status === 'ready').length
+    : orders.length;
+  console.log(filtered);
 }
--- ch6/6-11.js
+++ ch6/6-11.js
@@ -1,16 +1,29 @@
+//일단은 주어진 함수에서 여러 함수로 쪼깨기 작업을 했지만,
+//인자를 반복적으로 전달하고 있어서 Order라는 클래스로 공통적인 데이터를 묶어 연산해주는 것이 좋음
+
 export function priceOrder(product, quantity, shippingMethod) {
   const basePrice = product.basePrice * quantity;
-  const discount =
+  const discount = calculateDiscountedPrice(product, quantity);
+  const shippingCost = calculateShippingCost(
+    basePrice,
+    shippingMethod,
+    quantity
+  );
+  return basePrice - discount + shippingCost;
+}
+function calculateDiscountedPrice(product, quantity) {
+  return (
     Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
-    product.discountRate;
+    product.discountRate
+  );
+}
+function calculateShippingCost(basePrice, shippingMethod, quantity) {
   const shippingPerCase =
     basePrice > shippingMethod.discountThreshold
       ? shippingMethod.discountedFee
       : shippingMethod.feePerCase;
-  const shippingCost = quantity * shippingPerCase;
-  const price = basePrice - discount + shippingCost;
-  return price;
+  return quantity * shippingPerCase;
 }
 
 // 사용 예:
