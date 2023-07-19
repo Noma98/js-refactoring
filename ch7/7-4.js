fd0a0f571c6d04d2b768486b9c33b9d4541e1b7a
jinhee <wiostz98kr@gmail.com>
2023-07-19 20:59:08 +0900

    임시 변수를 질의 함수(getter)로 바꾸기

--- ch7/7-4.js
+++ ch7/7-4.js
@@ -1,3 +1,4 @@
+//문제점: Order의 basePrice, discountFactor가 얼마인지 궁금해도 알 수 없음
 class Order {
   #quantity;
   #item;
@@ -6,10 +7,15 @@ class Order {
     this.#item = item;
   }
 
+  //특정한 함수나 코드 블럭 속에서 사용하고 있는 임시 변수를 질의 함수(getter)로 만들어 둠으로써 활용성을 높일 수 있음
+  get basePrice() {
+    return this.#quantity * this.#item.price;
+  }
+  get discountFactor() {
+    return this.basePrice > 1000 ? 0.95 : 0.98;
+  }
+
   get price() {
-    const basePrice = this.#quantity * this.#item.price;
-    const discountFactor = 0.98;
-    if (basePrice > 1000) discountFactor -= 0.03;
-    return basePrice * discountFactor;
+    return this.basePrice * this.discountFactor;
   }
 }
