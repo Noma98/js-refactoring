471a8bdb34aed463a0db5187036917628281c18a
Noma <wiostz98kr@gmail.com>
2023-07-22 15:31:40 +0900

    필드 옮기기

--- ch8/8-2-1.js
+++ ch8/8-2-1.js
@@ -1,24 +1,21 @@
 export class Customer {
+  //고객에는 name과 contract에 대한 것만 가지고 discountRate은 계약 안으로 들어가는게 좀더 적합함
+  // #discountRate;
   #name;
-  #discountRate;
   #contract;
   constructor(name, discountRate) {
+    // this.#discountRate = discountRate;
     this.#name = name;
-    this.#discountRate = discountRate;
-    this.#contract = new CustomerContract(this.dateToday());
-  }
-
-  get discountRate() {
-    return this.#discountRate;
+    this.#contract = new CustomerContract(this.dateToday(), discountRate);
   }
 
   becomePreferred() {
-    this.#discountRate += 0.03;
+    this.#contract.discountRate += 0.03;
     // 다른 코드들이 있음...
   }
 
   applyDiscount(amount) {
-    return amount.subtract(amount.multiply(this.#discountRate));
+    return amount.subtract(amount.multiply(this.#contract.discountRate));
   }
 
   dateToday() {
@@ -28,7 +25,15 @@ export class Customer {
 
 class CustomerContract {
   #startDate;
-  constructor(startDate) {
+  #discountRate;
+  constructor(startDate, discountRate) {
     this.#startDate = startDate;
+    this.#discountRate = discountRate;
+  }
+  get discountRate() {
+    return this.#discountRate;
+  }
+  set discountRate(value) {
+    this.#discountRate = value;
   }
 }
