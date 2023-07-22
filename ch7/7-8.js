b217ccf57dc6d74c8837ab41c9d3f5a673c1a6e3
Noma <wiostz98kr@gmail.com>
2023-07-22 11:57:02 +0900

    중개자 제거하기

---
+++ ch7/7-8.js
@@ -0,0 +1,26 @@
+//7-7.js 중개자 제거한 버전
+class Person {
+  #name;
+  #manager;
+  #chargeCode;
+  constructor(name, manager, chargeCode) {
+    this.#name = name;
+    this.#manager = manager;
+    this.#chargeCode = chargeCode;
+  }
+
+  get name() {
+    return this.#name;
+  }
+  get manager() {
+    return this.#manager;
+  }
+  get chargeCode() {
+    return this.#chargeCode;
+  }
+}
+
+const person = new Person('Tom', 'aManager', '999');
+console.log(person.name);
+console.log(person.manager);
+console.log(person.chargeCode);
