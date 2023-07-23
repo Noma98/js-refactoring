b19a41f39e672108d181c76e25827ddae82b1a67
Noma <wiostz98kr@gmail.com>
2023-07-23 13:02:28 +0900

    필드 이름 바꾸기

--- ch9/9-2.js
+++ ch9/9-2.js
@@ -1,19 +1,23 @@
+//클래스 또는 객체 안에 있는 필드의 이름은 중요하기 때문에 정확하게 의도나 값을 나타낼 수 있는 단어로 지정해야 함
 class Organization {
+  #name;
+  #country;
   constructor(data) {
-    this._name = data.name;
-    this._country = data.country;
+    //private field는 _이 아닌 # 문법 사용하기
+    this.#name = data.name;
+    this.#country = data.country;
   }
-  get name() {
-    return this._name;
+  get title() {
+    return this.#name;
   }
-  set name(value) {
-    this._name = value;
+  set title(value) {
+    this.#name = value;
   }
   get country() {
-    return this._country;
+    return this.#country;
   }
   set country(value) {
-    this._country = value;
+    this.#country = value;
   }
 }
 const organization = new Organization({
