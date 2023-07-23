51712a5ad9ecc9a806cfa8de4cbaadf8a8b52f92
Noma <wiostz98kr@gmail.com>
2023-07-23 11:13:09 +0900

    문장 슬라이드하기

--- ch8/8-6.js
+++ ch8/8-6.js
@@ -1,18 +1,18 @@
+// 문장 슬라이드 : 관련 있는 코드들을 한 곳으로 뭉치도록 이동
+
 // 예제 1
+// 변수는 쓰이는 곳 가까이에 선언하기
 const pricingPlan = retrievePricingPlan();
+const chargePerUnit = pricingPlan.unit;
 const order = retreiveOrder();
 let charge;
-const chargePerUnit = pricingPlan.unit;
 
 // 예제 2
 function someFunc() {
-  let result;
-  if (availableResources.length === 0) {
-    result = createResource();
-    allocatedResources.push(result);
-  } else {
-    result = availableResources.pop();
-    allocatedResources.push(result);
-  }
+  const result =
+    availableResources.length === 0
+      ? createResource()
+      : availableResources.pop();
+  allocatedResources.push(result);
   return result;
 }
