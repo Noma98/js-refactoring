4405d6845b8dd7582ddbaa53ff690a446185d683
jinhee <wiostz98kr@gmail.com>
2023-07-24 19:34:40 +0900

    파생 변수를 질의 함수로 바꾸기

--- ch9/9-3.js
+++ ch9/9-3.js
@@ -1,13 +1,31 @@
 // 예제 1
 class Order {
+  /**
   // 다른 코드 있다고 가정
   get discountedTotal() {
     return this._discountedTotal;
   }
-  set discount(value) {
+  
+  //⛔️ discount를 set할 뿐인데 다른 필드도 업데이트 하고 있음
+  set discount(value) { 
     const old = this._discount;
     this._discount = value;
     this._discountedTotal += old - value;
+  }
+  get discountedTotal() {
+    return this._discountedTotal;
+    return this._basePrice - this._discount;
+  }
+  */
+  /*
+ - 👍 필요한 것만 업데이트하고 해당 값과 관련되어 실시간으로 계산되어야 하는 값은 질의함수로 만드는 것이 좋음
+ - 질의 함수: 연산을 통해 값을 계산하여 반환하는 함수. 질의는 내부 또는 외부의 값을 변경하여 부수 효과를 만들어선 안됨
+ */
+  get discountedTotal() {
+    return this._basePrice - this._discount;
+  }
+  set discount(value) {
+    this._discount = value;
   }
 }
 
@@ -15,10 +33,14 @@ class Order {
 class ProductionPlan {
   // 다른 코드 있다고 가정
   get production() {
-    return this._production;
+    // return this._production;
+    return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
+  }
+  get adjustment() {
+    return adjust;
   }
   applyAdjustment(adjustment) {
     this._adjustments.push(adjustment);
-    this._production += adjustment.amount;
+    // this._production += adjustment.amount;
   }
 }
