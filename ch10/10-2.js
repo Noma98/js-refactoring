4f7b197a81b517c922b9fb3b507ab2e990ac6bf4
jinhee <wiostz98kr@gmail.com>
2023-07-26 20:35:37 +0900

    조건식 통합하기

--- ch10/10-2.js
+++ ch10/10-2.js
@@ -1,6 +1,16 @@
+/**
+ * 동일한 동작을 수행하는 다양한 조건들이 있다면 이를 하나의 조건문으로 뭉친 다음 알맞은 이름(의도)을 붙이자.
+ */
 function disabilityAmount(employee) {
-  if (employee.seniority < 2) return 0;
-  if (employee.monthsDisabled > 12) return 0;
-  if (employee.isPartTime) return 0;
+  if (isNotEligibleForDisability(employee)) {
+    return 0;
+  }
   return 1;
+}
+function isNotEligibleForDisability(employee) {
+  return (
+    employee.seniority < 2 ||
+    employee.monthsDisabled > 12 ||
+    employee.isPartTime
+  );
 }
