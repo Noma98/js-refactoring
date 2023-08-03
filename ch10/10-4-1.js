869ad91c510a30ca8893d75da5a4c129d3f01fcc
jinhee <wiostz98kr@gmail.com>
2023-08-03 20:31:09 +0900

    조건부 로직을 다형성으로 바꾸기

--- ch10/10-4-1.js
+++ ch10/10-4-1.js
@@ -1,32 +1,65 @@
+/**
+ * 특정 객체의 타입에 따라 뭔가 동작하게 하려면 switch 타입을 남발하는 것이 아니라 
+   객체를 클래스로 만들어 다형성을 이용하도록 하는 것이 좋음
+ * switch 문을 남발하면 나중에 새로운 타입이 추가되었을 때 switch문을 하나하나 찾아다니면서 추가하고 유지보수 해야함
+ * 하지만 클래스로 만들어 두면, 새로운 새 종류가 생기면 그냥 클래스를 하나 더 만들면 되고, 특정 부분이 문제가 된다면 그 부분만 수정해주면 됨
+ */
 export function plumages(birds) {
-  let map = birds.map((b) => [b.name, plumage(b)]);
+  let map = birds.map((b) => [b.name, b.plumage]);
   let map1 = new Map(map);
   return map1;
 }
 export function speeds(birds) {
-  return new Map(birds.map((b) => [b.name, airSpeedVelocity(b)]));
+  return new Map(birds.map((b) => [b.name, b.airSpeedVelocity]));
 }
-export function plumage(bird) {
-  switch (bird.type) {
-    case 'EuropeanSwallow':
-      return 'average';
-    case 'AfricanSwallow':
-      return bird.numberOfCoconuts > 2 ? 'tired' : 'average';
-    case 'NorwegianBlueParrot':
-      return bird.voltage > 100 ? 'scorched' : 'beautiful';
-    default:
-      return 'unknown';
+class Bird {
+  #name;
+  constructor(name) {
+    this.#name = name;
+  }
+  get name() {
+    return this.#name;
+  }
+  get plumage() {
+    return 'unknown';
+  }
+  get airSpeedVelocity() {
+    return null;
   }
 }
-export function airSpeedVelocity(bird) {
-  switch (bird.type) {
-    case 'EuropeanSwallow':
-      return 35;
-    case 'AfricanSwallow':
-      return 40 - 2 * bird.numberOfCoconuts;
-    case 'NorwegianBlueParrot':
-      return bird.isNailed ? 0 : 10 + bird.voltage / 10;
-    default:
-      return null;
+class EuropeanSwallow extends Bird {
+  constructor() {
+    super('EuropeanSwallow');
+  }
+  get plumage() {
+    return 'average';
+  }
+  get airSpeedVelocity() {
+    return 35;
   }
 }
+
+class AfricanSwallow extends Bird {
+  constructor() {
+    super('AfricanSwallow');
+  }
+  get plumage() {
+    return this.numberOfCoconuts > 2 ? 'tired' : 'average';
+  }
+  get airSpeedVelocity() {
+    return 40 - 2 * this.numberOfCoconuts;
+  }
+}
+class NorwegianBlueParrot extends Bird {
+  constructor() {
+    super('NorwegianBlueParrot');
+  }
+  get plumage() {
+    return this.voltage > 100 ? 'scorched' : 'beautiful';
+  }
+  get airSpeedVelocity() {
+    return this.isNailed ? 0 : 10 + this.voltage / 10;
+  }
+}
+const result = plumages([new NorwegianBlueParrot(), new AfricanSwallow()]);
+console.log(result);
--- ch10/10-4-2.js
+++ ch10/10-4-2.js
@@ -1,52 +1,75 @@
+/**
+ * 등급을 계산할때 반복적으로 나타나는 조건문이 있음
+   - 중국을 거치는지, history 안에 중국이 있는지에 따라 계산하는 로직이 조금씩 달라짐
+ * 그래서 Rating이라는 클래스를 만들어서 여기서 관련된 voyage와 history로 점수를 만들고, 
+   중국과 관련된 위험 부담이 있는 클래스(ExperiencedChinaRating)를 따로 만들어서
+   다른 rating을 계산하도록 만들면 다형성을 극대화할 수 있음
+ * 상속이라는 것이 새, 동물, 사람과 같은 물질적인 것들 뿐만 아니라 점수, 등급과 같은 것들도 
+   클래스로 만들어서 서로 공통되는 부분은 하나의 추상 클래스, 부모 클래스로 묶어 두고
+   다른 부분만 따로 빼 오버라이딩하면서 다른 계산을 하도록 만들면!
+   조건문을 굳이 사용하지 않아도 다형성만으로도 깔끔하게 만들 수 있음
+ */
+
 export function rating(voyage, history) {
-  // 투자 등급
-  const vpf = voyageProfitFactor(voyage, history);
-  const vr = voyageRisk(voyage);
-  const chr = captainHistoryRisk(voyage, history);
-  if (vpf * 3 > vr + chr * 2) return 'A';
-  else return 'B';
-}
-
-function voyageRisk(voyage) {
-  // 항해 경로 위험요소
-  let result = 1;
-  if (voyage.length > 4) result += 2;
-  if (voyage.length > 8) result += voyage.length - 8;
-  if (['china', 'east-indies'].includes(voyage.zone)) result += 4;
-  return Math.max(result, 0);
-}
-
-function captainHistoryRisk(voyage, history) {
-  // 선장의 항해 이력 위험 요소
-  let result = 1;
-  if (history.length < 5) result += 4;
-  result += history.filter((v) => v.profit < 0).length;
-  if (voyage.zone === 'china' && hasChina(history)) result -= 2;
-  return Math.max(result, 0);
-}
-
-function hasChina(history) {
-  // 중국을 경유하는가?
-  return history.some((v) => 'china' === v.zone);
-}
-
-function voyageProfitFactor(voyage, history) {
-  // 수익 요인
-  let result = 2;
-  if (voyage.zone === 'china') result += 1;
-  if (voyage.zone === 'east-indies') result += 1;
-  if (voyage.zone === 'china' && hasChina(history)) {
-    result += 3;
-    if (history.length > 10) result += 1;
-    if (voyage.length > 12) result += 1;
-    if (voyage.length > 18) result -= 1;
-  } else {
-    if (history.length > 8) result += 1;
-    if (voyage.length > 14) result -= 1;
+  if (this.voyage.zone === 'china' && history.some((v) => 'china' === v.zone)) {
+    return new ExperiencedChinaRating(voyage, history).value;
   }
-  return result;
+  return new Rating(voyage, history).value;
 }
+class Rating {
+  constructor(voyage, history) {
+    this.voyage = voyage;
+    this.history = history;
+  }
+  get value() {
+    const profit = this.voyageProfitFactor;
+    const risk = this.voyageRisk;
+    const historyRisk = this.captainHistoryRisk;
+    return profit * 3 > risk + historyRisk * 2 ? 'A' : 'B';
+  }
+  get voyageRisk() {
+    let result = 1;
+    if (this.voyage.length > 4) result += 2;
+    if (this.voyage.length > 8) result += this.voyage.length - 8;
+    if (['china', 'east-indies'].includes(this.voyage.zone)) result += 4;
+    return Math.max(result, 0);
+  }
 
+  get captainHistoryRisk() {
+    let result = 1;
+    if (this.history.length < 5) result += 4;
+    result += this.history.filter((v) => v.profit < 0).length;
+
+    return Math.max(result, 0);
+  }
+
+  get voyageProfitFactor() {
+    let result = 2;
+    if (this.voyage.zone === 'china') result += 1;
+    if (this.voyage.zone === 'east-indies') result += 1;
+    result += this.voyageHistoryAndLengthFactor;
+    return result;
+  }
+  get voyageHistoryAndLengthFactor() {
+    let result = 0;
+    if (this.history.length > 8) result += 1;
+    if (this.voyage.length > 14) result -= 1;
+    return result;
+  }
+}
+class ExperiencedChinaRating extends Rating {
+  get captainHistoryRisk() {
+    const result = super.captainHistoryRisk - 2;
+    return Math.max(result, 0);
+  }
+  get voyageHistoryAndLengthFactor() {
+    let result = 3;
+    if (this.history.length > 10) result += 1;
+    if (this.voyage.length > 12) result += 1;
+    if (this.voyage.length > 18) result -= 1;
+    return result;
+  }
+}
 const voyage = { zone: 'west-indies', length: 10 };
 const history = [
   { zone: 'east-indies', profit: 5 },
