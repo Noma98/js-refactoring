1b18507d812bc79c71b97ed482010b03bfaffdd8
jinhee <wiostz98kr@gmail.com>
2023-08-08 21:18:25 +0900

    질의 함수와 변경 함수 분리하기

--- ch11/11-1.js
+++ ch11/11-1.js
@@ -1,28 +1,27 @@
-// 예제 1
-function totalOutstandingAndSendBill() {
-  const result = customer.invoices.reduce(
-    (total, each) => each.amount + total,
-    0
-  );
-  sendBill();
-  return result;
+// 예제 1 - and로 묶어 2가지 일 하지 말고 하나에 하나만 하도록 분리
+function getTotalOutstanding() {
+  return customer.invoices.reduce((total, each) => each.amount + total, 0);
+}
+function sendBill() {
+  //bill 보내기..
 }
 
-// 예제 2
+// 예제 2 - 부수적인 효과는 없지만 악당 찾기, 알람 울리기 총 2가지 일을 하고 있음 -> 분리해주기
 export function alertForMiscreant(people, alarm) {
+  const miscreant = findMiscreant(people);
+  setOffAlarms(alarm, miscreant);
+}
+function findMiscreant(people) {
   for (const p of people) {
     if (p === 'Don') {
-      setOffAlarms(alarm, p);
       return 'Don';
     }
     if (p === 'John') {
-      setOffAlarms(alarm, p);
       return 'John';
     }
   }
   return '';
 }
-
 function setOffAlarms(alarm, p) {
   alarm.setOff('Found Miscreant ' + p);
 }
