b7462fce812e32ab9fbb2ed17d074b47bee89638
jinhee <wiostz98kr@gmail.com>
2023-08-08 20:29:59 +0900

    제어 플래그를 탈출문으로 바꾸기

--- ch10/10-7.js
+++ ch10/10-7.js
@@ -1,8 +1,15 @@
+/**
+- 불리언 플래그를 쓰는 대신에 함수 상단에서 특정 조건이 되면 바로 리턴될 수 있게 하는 것이 좋음
+ */
 for (const p of people) {
-  if (!found) {
-    if (p === 'Don') {
-      sendAlert();
-      found = true;
-    }
+  // if (!found) {
+  //   if (p === 'Don') {
+  //     sendAlert();
+  //     found = true;
+  //   }
+  // }
+  if (p === 'Don') {
+    sendAlert();
+    break;
   }
 }
