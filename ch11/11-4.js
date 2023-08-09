dfb87e1322879e684313e36a05098d3cc81fae88
jinhee <wiostz98kr@gmail.com>
2023-08-09 21:02:36 +0900

    객체 통째로 넘기기

--- ch11/11-4.js
+++ ch11/11-4.js
@@ -1,8 +1,8 @@
 export function temperatureAlerts(room, plan) {
   const alerts = [];
-  const low = room.daysTempRange.low;
-  const high = room.daysTempRange.high;
-  if (!plan.withinRange(low, high)) {
+  // const low = room.daysTempRange.low;
+  // const high = room.daysTempRange.high;
+  if (!plan.withinRange(room.daysTempRange)) {
     alerts.push('room temperature went outside range');
   }
 
@@ -14,9 +14,10 @@ export class HeatingPlan {
     this._temperatureRange = temperatureRange;
   }
 
-  withinRange(bottom, top) {
+  withinRange(range) {
     return (
-      bottom >= this._temperatureRange.low && top <= this._temperatureRange.high
+      range.bottom >= this._temperatureRange.low &&
+      range.top <= this._temperatureRange.high
     );
   }
 }
