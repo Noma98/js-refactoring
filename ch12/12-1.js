d8aefbc777936e081d1a258661dccb169fde653d
jinhee <wiostz98kr@gmail.com>
2023-08-14 19:36:10 +0900

    부모 클래스로 메서드 올리기

--- ch12/12-1.js
+++ ch12/12-1.js
@@ -1,24 +1,23 @@
 // 예시 1
-class Employee {}
+// 서브 클래스에서 공통된 속성이나 행동을 가지고 있다면, 부모 클래스로 올려주는게 중복을 방지 할 수 있고 공통된 규격을 약속할 수 있음
+class Employee {
+  get name() {}
+}
 
 class Salesperson extends Employee {
-  get name() {}
+  // get name() {}
 }
 
 class Engineer extends Employee {
-  get name() {}
+  // get name() {}
 }
 
 // 예시 2
-class Party {}
-
-class Department extends Party {
-  get totalAnnualCost() {
-    return this.monthlyCost * 12;
-  }
-}
-class Employee extends Party {
+class Party {
   get annualCost() {
     return this.monthlyCost * 12;
   }
 }
+
+class Department extends Party {}
+class Employee extends Party {}
