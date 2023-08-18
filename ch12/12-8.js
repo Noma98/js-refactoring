8ec3ec550784988471bd437078bb0dc9ed5d0b2b
jinhee <wiostz98kr@gmail.com>
2023-08-18 19:45:40 +0900

    슈퍼 클래스 추출하기

--- ch12/12-8.js
+++ ch12/12-8.js
@@ -1,11 +1,14 @@
-class Department {
-  get totalAnnualCost() {}
+/**
+ * name과 annualCost가 공통됨 -> 부모 클래스를 만들어서 공통되는 부분을 재사용
+ */
+class Party {
   get name() {}
+  get annualCost() {}
+}
+class Department extends Party {
   get headCount() {}
 }
 
-class Employee {
-  get annualCost() {}
-  get name() {}
+class Employee extends Party {
   get id() {}
 }
