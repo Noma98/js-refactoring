5d77026e0a9dece044d4187403fdac9c13b9c7fe
jinhee <wiostz98kr@gmail.com>
2023-08-18 19:39:55 +0900

    서브 클래스 제거하기

--- ch12/12-7.js
+++ ch12/12-7.js
@@ -1,7 +1,11 @@
+// 각각의 서브 클래스별로 이용할 만한 다형성이나 유스케이스들이 없다면 그냥 하나의 클래스로 남겨두는 게 나을 때도 있음
+// -> 그럴 땐 불필요한 서브 클래스를 제거하고, 외부에서 알 필요 없는 내부 로직들을 캡슐화 해두고, 외부에서 사용하기 쉽게 팩토리 메서드를 제공해줄 수 있음
 class Person {
   #name;
-  constructor(name) {
+  #genderCode;
+  constructor(name, genderCode) {
     this.#name = name;
+    this.#genderCode = genderCode;
   }
 
   get name() {
@@ -9,37 +13,28 @@ class Person {
   }
 
   get genderCode() {
-    return 'X';
+    return this.#genderCode;
   }
-}
 
-class Male extends Person {
-  get genderCode() {
-    return 'M';
+  get isMale() {
+    return this.#genderCode === 'M';
   }
-}
-
-class Female extends Person {
-  get genderCode() {
-    return 'F';
+  static create(record) {
+    switch (record.gender) {
+      case 'M':
+        return new Person(record.name, 'M');
+      case 'F':
+        return new Person(record.name, 'F');
+      default:
+        return new Person(record.name, 'X');
+    }
   }
 }
 
 function loadFromInput(data) {
   const result = [];
   data.forEach((record) => {
-    let person;
-    switch (record.gender) {
-      case 'M':
-        person = new Male(record.name);
-        break;
-      case 'F':
-        person = new Female(record.name);
-        break;
-      default:
-        person = new Person(record.name);
-    }
-    result.push(person);
+    result.push(Person.create(record));
   });
   return result;
 }
@@ -49,5 +44,5 @@ const people = loadFromInput([
   { name: '철수', gender: 'M' },
   { name: '밥', gender: 'M' },
 ]);
-const numberOfMales = people.filter((p) => p instanceof Male).length;
+const numberOfMales = people.filter((p) => p.isMale).length;
 console.log(numberOfMales);
