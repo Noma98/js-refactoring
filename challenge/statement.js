f0d20285d0171b8e60e78a8340e490869ce3ed19
Noma <wiostz98kr@gmail.com>
2023-09-07 21:06:32 +0900

    새로운 기능 + 테스트 추가

--- challenge/statement.js
+++ challenge/statement.js
@@ -1,61 +1,23 @@
-export function statement(invoice, plays) {
-  const statement = createStatement(invoice, plays);
-  return renderPlainText(statement);
-}
-function createStatement(invoice, plays) {
-  const statement = {};
-  statement.customer = invoice.customer;
-  statement.performances = invoice.performances.map(enrichPerformance);
-  statement.totalAmount = totalAmount(statement.performances);
-  statement.totalCredits = totalCredits(statement.performances);
-  return statement;
+import { createStatement } from './create_statement.js';
 
-  function enrichPerformance(performance) {
-    const result = { ...performance };
-    result.play = playFor(performance);
-    result.amount = amountFor(result);
-    result.credits = creditsFor(result);
-    return result;
+export function statement(invoice, plays) {
+  return renderPlainText(createStatement(invoice, plays));
+}
+export function htmlStatement(invoice, plays) {
+  return renderHTML(createStatement(invoice, plays));
+}
+function renderHTML(statement) {
+  let result = `<h1>청구 내역 (고객명: ${statement.customer})</h1>\n`;
+  result += `<table>\n`;
+  result += `<tr><th>play</th><th>석</th><th>cost</th></tr>`;
+  for (let perf of statement.performances) {
+    result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${usd(perf.amount / 100)}</td></tr>\n`;
   }
-  function playFor(performance) {
-    return plays[performance.playID];
-  }
-  function amountFor(performance) {
-    let result = 0;
-    switch (performance.play.type) {
-      case 'tragedy': // 비극
-        result = 40000;
-        if (performance.audience > 30) {
-          result += 1000 * (performance.audience - 30);
-        }
-        break;
-      case 'comedy': // 희극
-        result = 30000;
-        if (performance.audience > 20) {
-          result += 10000 + 500 * (performance.audience - 20);
-        }
-        result += 300 * performance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${performance.play.type}`);
-    }
-    return result;
-  }
-  function creditsFor(performance) {
-    let result = 0;
-    result = Math.max(performance.audience - 30, 0);
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ('comedy' === performance.play.type) {
-      result += Math.floor(performance.audience / 5);
-    }
-    return result;
-  }
-  function totalAmount(performances) {
-    return performances.reduce((sum, p) => (sum += p.amount), 0);
-  }
-  function totalCredits(performances) {
-    return performances.reduce((sum, p) => (sum += p.credits), 0);
-  }
+  result += '</table>\n';
+  result += `<p>총액: <em>${usd(statement.totalAmount / 100)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${statement.totalCredits}</em>점</p>\n`;
+  return result;
 }
 function renderPlainText(statement) {
   let result = `청구 내역 (고객명: ${statement.customer})\n`;
